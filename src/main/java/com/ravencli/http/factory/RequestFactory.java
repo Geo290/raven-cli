db17976dbbaf2f7bd2d2907a8ef4ac3c75ac901d
Geo290 <jorgeemmanuel.2003@gmail.com>
2025-05-23 14:37:54 -0600

    feature: factory created

--- src/main/java/com/ravencli/http/factory/RequestFactory.java
+++ src/main/java/com/ravencli/http/factory/RequestFactory.java
@@ -1,5 +1,33 @@
 package com.ravencli.http.factory;
 
-public class RequestFactory {
+import java.net.URI;
+import java.net.http.HttpRequest;
 
+import org.json.JSONObject;
+
+import com.ravencli.http.factory.abstracted.concreteProducts.*;
+
+public class RequestFactory {
+    private String requestType;
+
+    public RequestFactory(String requestType) {
+        this.requestType = requestType;
+    }
+
+    public HttpRequest createRequest(URI uri, JSONObject body) {
+        String stringifiedType = this.requestType.toString();
+
+        switch (stringifiedType) {
+            case "POST":
+                return new PostRequest(uri, body).build();
+            case "GET":
+                return new GetRequest(uri).build();
+            case "PUT":
+                return new PutRequest(uri, body).build();
+            case "DELETE":
+                return new DeleteRequest(uri).build();
+            default:
+                throw new IllegalArgumentException("Invalid request type: " + stringifiedType);
+        }
+    }
 }
