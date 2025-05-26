9739d0e25994bc7d622928bae8c8b819a6b35e86
Geo290 <jorgeemmanuel.2003@gmail.com>
2025-05-26 15:56:08 -0600

    refactor: buildRequest() method overloaded to accept two or three parameters

--- src/main/java/com/ravencli/http/client/RestClient.java
+++ src/main/java/com/ravencli/http/client/RestClient.java
@@ -1,22 +1,30 @@
 package com.ravencli.http.client;
 
 import java.io.IOException;
+
+import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+
+import org.json.JSONObject;
 
 import com.ravencli.http.factory.RequestFactory;
 
 public class RestClient {
     private HttpClient client = HttpClient.newHttpClient();
     private RequestFactory factory;
+    
     public RestClient() {
         this.factory = new RequestFactory();
     }
 
-    public HttpRequest buildRequest(String requestType) {
-        HttpRequest request = factory.createRequest(requestType, null);
-        return request;
+    public HttpRequest buildRequest(String requestType, URI uri, JSONObject body) {
+        return factory.createRequest(requestType, uri, body);
+    }
+
+    public HttpRequest buildRequest(String requestType, URI uri) {
+        return factory.createRequest(requestType, uri);
     }
 
     public void execute(HttpRequest request) {
