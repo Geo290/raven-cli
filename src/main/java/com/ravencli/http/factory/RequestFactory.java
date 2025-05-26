77a7ce199d7cf729e457a86e05ff7b4df05ebcc0
Geo290 <jorgeemmanuel.2003@gmail.com>
2025-05-26 12:44:47 -0600

    chore: Doc comments added

--- src/main/java/com/ravencli/http/factory/RequestFactory.java
+++ src/main/java/com/ravencli/http/factory/RequestFactory.java
@@ -7,27 +7,64 @@ import org.json.JSONObject;
 
 import com.ravencli.http.factory.abstracted.concreteProducts.*;
 
+/**
+ * Factory class to create HTTP request based on an identified type
+ * (e.g., POST, PUT, GET, DELETE).
+ * This class encapsulates the logic for creating different types of HTTP
+ */
 public class RequestFactory {
-    private String requestType;
 
-    public RequestFactory(String requestType) {
-        this.requestType = requestType;
+    public RequestFactory() {
     }
 
-    public HttpRequest createRequest(URI uri, JSONObject body) {
-        String stringifiedType = this.requestType.toString();
+    /**
+     * Creates a request based on the http method, API URI, and request body.
+     * 
+     * @param requestType: POST, PUT
+     * @param uri:         URI
+     * @param body:        JSONObject
+     * @return PostRquest, PutRequest
+     * @throws IllegalArgumentException
+     */
+    public HttpRequest createRequest(String requestType, URI uri, JSONObject body) {
+        if (body == null || body.isEmpty()) {
+            throw new IllegalArgumentException("Body cannot be null or empty");
+        }
+        if (uri == null) {
+            throw new IllegalArgumentException("URI cannot be null or empty");
+        }
 
-        switch (stringifiedType) {
+        switch (requestType.toUpperCase()) {
             case "POST":
                 return new PostRequest(uri, body).build();
-            case "GET":
-                return new GetRequest(uri).build();
             case "PUT":
                 return new PutRequest(uri, body).build();
+            default:
+                throw new IllegalArgumentException("Invalid request type: " + requestType);
+        }
+    }
+
+    /**
+     * Creates a request based on the http mehtod and API URUI.
+     * 
+     * @param requestType: GET, DELETE
+     * @param uri:         URI
+     * @param body:        JSONObject
+     * @return PostRquest, PutRequest
+     * @throws IllegalArgumentException
+     */
+    public HttpRequest createRequest(String requestType, URI uri) {
+        if (uri == null) {
+            throw new IllegalArgumentException("URI cannot be null or empty");
+        }
+        
+        switch (requestType) {
+            case "GET":
+                return new GetRequest(uri).build();
             case "DELETE":
                 return new DeleteRequest(uri).build();
             default:
-                throw new IllegalArgumentException("Invalid request type: " + stringifiedType);
+                throw new IllegalArgumentException("Invalid request type: " + requestType);
         }
     }
 }
