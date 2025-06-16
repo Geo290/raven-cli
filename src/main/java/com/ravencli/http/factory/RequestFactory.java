1916d88fa284ba8ab011155ec75b7d1301d37bdc
Geo290 <jorgeemmanuel.2003@gmail.com>
2025-06-15 20:24:32 -0600

    refactor: removed unnecesary empty request body validation

--- src/main/java/com/ravencli/http/factory/RequestFactory.java
+++ src/main/java/com/ravencli/http/factory/RequestFactory.java
@@ -20,51 +20,52 @@ public class RequestFactory {
     /**
      * Creates a request based on the http method, API URI, and request body.
      * 
-     * @param requestType: POST, PUT
-     * @param uri:         URI
-     * @param body:        JSONObject
+     * @param requestMethod: POST, PUT
+     * @param uri:           URI
+     * @param body:          JSONObject
      * @return PostRquest, PutRequest
      * @throws IllegalArgumentException
      */
-    public HttpRequest createRequest(String requestType, URI uri, JSONObject body) {
-        if (body == null || body.isEmpty()) {
-            throw new IllegalArgumentException("Body cannot be null or empty");
-        }
+    public HttpRequest createRequest(String requestMethod, URI uri, JSONObject body) {
         if (uri == null) {
             throw new IllegalArgumentException("URI cannot be null or empty");
         }
+        if (body == null || body.isEmpty()) {
+            throw new IllegalArgumentException("Body cannot be null or empty");
+        }
 
-        switch (requestType.toUpperCase()) {
+        switch (requestMethod.toUpperCase()) {
             case "POST":
                 return new PostRequest(uri, body).build();
             case "PUT":
                 return new PutRequest(uri, body).build();
             default:
-                throw new IllegalArgumentException("Invalid request type: " + requestType);
+                throw new IllegalArgumentException("Invalid request type: " + requestMethod);
         }
     }
 
+    
     /**
      * Creates a request based on the http mehtod and API URUI.
      * 
-     * @param requestType: GET, DELETE
-     * @param uri:         URI
-     * @param body:        JSONObject
+     * @param requestMethod: GET, DELETE
+     * @param uri:           URI
+     * @param body:          JSONObject
      * @return PostRquest, PutRequest
      * @throws IllegalArgumentException
      */
-    public HttpRequest createRequest(String requestType, URI uri) {
+    public HttpRequest createRequest(String requestMethod, URI uri) {
         if (uri == null) {
             throw new IllegalArgumentException("URI cannot be null or empty");
         }
-        
-        switch (requestType) {
+
+        switch (requestMethod.toUpperCase()) {
             case "GET":
                 return new GetRequest(uri).build();
             case "DELETE":
                 return new DeleteRequest(uri).build();
             default:
-                throw new IllegalArgumentException("Invalid request type: " + requestType);
+                throw new IllegalArgumentException("Invalid request type: " + requestMethod);
         }
     }
 }
