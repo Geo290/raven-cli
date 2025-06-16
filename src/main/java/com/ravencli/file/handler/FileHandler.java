50b01f49e1c2ad9b60d14db3e649e28b9d41f961
Geo290 <jorgeemmanuel.2003@gmail.com>
2025-06-15 20:23:26 -0600

    refactor: changed all keys to match with test case notation

--- src/main/java/com/ravencli/file/handler/FileHandler.java
+++ src/main/java/com/ravencli/file/handler/FileHandler.java
@@ -1,24 +1,18 @@
 package com.ravencli.file.handler;
 
 import java.net.URI;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 import org.json.JSONObject;
 
 import com.ravencli.file.model.TestCase;
 
 public class FileHandler {
-    private Path fileUrl;
-
-    public FileHandler(Path fileUrl) {
-        this.fileUrl = fileUrl;
-    }
-
-    protected JSONObject getJsonObject() {
+    protected static JSONObject getJsonObject(File fileUrl) {
         try {
-            String content = Files.readString(fileUrl);
+            String content = Files.readString(fileUrl.toPath());
             JSONObject jsonObject = new JSONObject(content);
             return jsonObject;
         } catch (IOException e) {
@@ -27,62 +21,58 @@ public class FileHandler {
         return null;
     }
 
-    protected URI getJsonObjectUri(JSONObject object) {
-        if (!object.has("API_URI")) {
-            throw new IllegalArgumentException("API_URI not found in JSON object");
+    protected static URI getJsonObjectApiUri(JSONObject object) {
+        if (!object.has("api_uri")) {
+            throw new IllegalArgumentException("api_uri not found in JSON object");
         }
 
-        if (object.getString("API_URI").isBlank()) {
-            throw new IllegalArgumentException("API_URI must not be black or empty");
+        if (object.getString("api_uri").isBlank()) {
+            throw new IllegalArgumentException("api_uri must not be black or empty");
         }
 
-        return URI.create(object.getString("API_URI"));
+        return URI.create(object.getString("api_uri"));
     }
 
-    protected String getJsonObjectMethod(JSONObject object) {
-        if (!object.has("METHOD")) {
-            throw new IllegalArgumentException("METHOD not found in JSON object");
+    protected static String getJsonObjectRequestMethod(JSONObject object) {
+        if (!object.has("request_method")) {
+            throw new IllegalArgumentException("request_method not found in JSON object");
         }
 
-        if (object.getString("METHOD").isBlank()) {
-            throw new IllegalArgumentException("METHOD must not be black or empty");
+        if (object.getString("request_method").isBlank()) {
+            throw new IllegalArgumentException("request_method must not be black or empty");
         }
 
-        return object.getString("METHOD").toUpperCase();
+        return object.getString("request_method").toUpperCase();
     }
 
-    protected JSONObject getJsonObjectRequestBody(JSONObject object) {
-        if (!object.has("REQUEST_BODY")) {
-            throw new IllegalArgumentException("REQUEST_BODY not found in JSON object");
+    protected static JSONObject getJsonObjectRequestBody(JSONObject object) {
+        if (!object.has("request_body")) {
+            throw new IllegalArgumentException("Request body not found in JSON object");
         }
-
-        if (object.getJSONObject("REQUEST_BODY").isEmpty()) {
-            throw new IllegalArgumentException("REQUEST_BODY must not be black or empty");
-        }
-        return object.getJSONObject("REQUEST_BODY");
+        return object.getJSONObject("request_body");
 
     }
 
-    protected JSONObject getJsonObjectExpectedResponse(JSONObject object) {
-        if (!object.has("EXPECTED_RESULTS")) {
-            throw new IllegalArgumentException("EXPECTED_RESULTS not found in JSON object");
+    protected static JSONObject getJsonObjectExpectedResponse(JSONObject object) {
+        if (!object.has("expected_response")) {
+            throw new IllegalArgumentException("expected_response not found in JSON object");
         }
 
-        if (object.getJSONObject("EXPECTED_RESULTS").isEmpty()) {
-            throw new IllegalArgumentException("EXPECTED_RESULTS must not be black or empty");
+        if (object.getJSONObject("expected_response").isEmpty()) {
+            throw new IllegalArgumentException("expected_response must not be black or empty");
         }
 
-        return object.getJSONObject("EXPECTED_RESULTS");
+        return object.getJSONObject("expected_response");
     }
 
-    public TestCase parse() throws IOException {
-        JSONObject jsonObject = getJsonObject();
+    public static TestCase parse(File file) throws IOException {
+        JSONObject jsonObject = getJsonObject(file);
 
-        URI apUri = getJsonObjectUri(jsonObject);
-        String method = getJsonObjectMethod(jsonObject);
+        URI apUri = getJsonObjectApiUri(jsonObject);
+        String requestMethod = getJsonObjectRequestMethod(jsonObject);
         JSONObject requestBody = getJsonObjectRequestBody(jsonObject);
         JSONObject expectedResponse = getJsonObjectExpectedResponse(jsonObject);
 
-        return new TestCase(apUri, method, requestBody, expectedResponse);
+        return new TestCase(apUri, requestMethod, requestBody, expectedResponse);
     }
 }
